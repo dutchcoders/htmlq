a12d93bf6d7ccabe12752ba6bca12c373dfb4105
Remco <remco@dutchcoders.io>
2015-05-05 09:52:11 +0200

    show attribute values

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 # htmlq
 
-Query HTML from StdIn using css selectors.
+A querying tool for html using css selectors.
 
 ## Example
 ```
-curl https://api.slack.com/rtm | htmlq "a[class='bold block']"
+curl https://api.slack.com/rtm | htmlq --filter "a[class='bold block']" @href
+curl https://api.slack.com/rtm | htmlq --filter img @src
+curl https://api.slack.com/rtm | htmlq --filter a
 ```
 
 ## Build 
 ```
 go build -o /usr/local/bin/htmlq main.go
 ```
-
-## Why 
-Sometimes it is necessary to get just some values from a webpage.
 
 ## Thanks 
 Many thanks to PuerkitoBio (https://github.com/PuerkitoBio/goquery). Without his library this tool would have been many more than the current few lines.
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	kingpin "gopkg.in/alecthomas/kingpin.v1"
 )
 
 func main() {
@@ -13,18 +14,29 @@ func main() {
 	if (stat.Mode() & os.ModeCharDevice) != 0 {
 		fmt.Println("htmlq: Query HTML from StdIn using css selectors")
 		fmt.Println("\nUsage:")
-		fmt.Println("curl https://www.google.com/ | htmlq \"a\"")
+		fmt.Println("curl https://www.google.com/ | htmlq --filter \"a\"")
 		return
 	}
+
+	filter := kingpin.Flag("filter", "filter").Short('f').Required().String()
+	arg := kingpin.Arg("arg", "arg").String()
+	kingpin.Parse()
 
 	doc, err := goquery.NewDocumentFromReader(os.Stdin)
 	if err != nil {
-		fmt.Println("Error %s", err)
+		fmt.Println("Coud not read input: %s", err.Error())
 		return
 	}
 
-	doc.Find(strings.Join(os.Args[1:], " ")).Each(func(i int, s *goquery.Selection) {
-		fmt.Println(s.Text())
-	})
+	doc.Find(*filter).Each(func(i int, s *goquery.Selection) {
+		if *arg == "" {
+			fmt.Println(strings.TrimSpace(s.Text()))
+			return
+		}
 
+		// attribute
+		if val, ok := s.Attr((*arg)[1:]); ok {
+			fmt.Println(val)
+		}
+	})
 }
